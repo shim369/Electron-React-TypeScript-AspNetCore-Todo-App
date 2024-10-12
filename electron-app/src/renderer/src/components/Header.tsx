12f1d0ec17311664be2a6eb6f0cdf08e8778b546
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-10-12 16:24:20 +0900

    test

--- electron-app/src/renderer/src/components/Header.tsx
+++ electron-app/src/renderer/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 function Header(): JSX.Element {
   return (
-    <header className="d-flex justify-content-between align-items-center p-4">
+    <header className="shadow d-flex justify-content-between align-items-center p-4">
       <h1 className="text-2xl font-bold">
         <Link to="/" className="text-dark text-decoration-none">
           Electron Todo App
